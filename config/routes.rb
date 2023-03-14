7210592d95110fc40d394685af4e98d6a12c0098
yonas44 <tesfu.yonas1350@gmail.com>
2023-03-14 15:13:39 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   root "recipes#index"
 
-  resources :foods, only: [:index, :new, :create, :edit, :destroy, :update]
+  resources :foods
   resources :recipes
   resources :users
 end
