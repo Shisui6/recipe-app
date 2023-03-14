f62505954ff580fd4ff2c7c4845caa3b4be16535
yonas44 <tesfu.yonas1350@gmail.com>
2023-03-14 13:44:04 +0300

    Add quantitiy in the foods table

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,4 @@ Rails.application.routes.draw do
   resources :foods, only: [:index, :new, :create, :edit, :destroy, :update]
   resources :recipes
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
